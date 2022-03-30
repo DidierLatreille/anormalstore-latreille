e707b23c5424d698c680f488a935dd1d78e0b193
DidierLatreille <didier.latreille@gmail.com>
2022-03-29 21:38:14 -0300

    AnormalStoreLatreille

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return (
+        <div className="NavBar">
+            <h1 className="StoreBar">AnormalStore</h1>
+            <ul className="MenuBar">
+                <li><a className="ElementoMenu" href="">Tienda</a></li>
+                <li><a className="ElementoMenu" href="">Acerca De</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default NavBar;
