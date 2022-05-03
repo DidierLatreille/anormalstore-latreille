89731a7ec204b253f77e2d16a016c0e31896da2e
DidierLatreille <didier.latreille@gmail.com>
2022-05-03 02:46:17 -0300

    anormalstore-latreille

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,9 +2,54 @@ import '../App.css';
 import { useContext } from 'react';
 import { CartContext } from './CartContext';
 import { Link } from 'react-router-dom';
+import { collection, doc, setDoc, serverTimestamp, updateDoc, increment } from "firebase/firestore";
+import db from './utils/firebaseConfig';
 
 const Cart = () => {
     const test = useContext(CartContext);
+
+    const checkout = () => {
+        const itemsForDB = test.cartList.map(item => ({
+          id: item.id,
+          title: item.title,
+          price: item.price,
+          qty: item.qtyItem
+        }));
+    
+        test.cartList.forEach(async (item) => {
+          const itemRef = doc(db, "products", item.id);
+          await updateDoc(itemRef, {
+            stock: increment(-item.qtyItem)
+          });
+        });
+    
+        let order = {
+          buyer: {
+            name: "Peron",
+            email: "peronperon@gmail.com",
+            phone: "1137081848"
+          },
+          total: test.totalCarro(),
+          items: itemsForDB,
+          date: serverTimestamp()
+        };
+    
+        console.log(order);
+    
+        const createOrderInFirestore = async () => {
+          // Add a new document with a generated id
+          const newOrderRef = doc(collection(db, "orders"));
+          await setDoc(newOrderRef, order);
+          return newOrderRef;
+        }
+    
+        createOrderInFirestore()
+          .then(result => alert('Your order has been created. Please take note of the ID of your order.\n\n\nOrder ID: ' + result.id + '\n\n'))
+          .catch(err => console.log(err));
+    
+        test.clear();
+    
+      }
 
     function subtotal(item){
         return item.price * item.qtyItem;
@@ -53,7 +98,7 @@ const Cart = () => {
             <div className='resumenCompra'>
                 <h2 className='tituloResumenCompra'>Resumen de compra</h2>
                 <h3>Total Carrito: {test.totalCarro()}</h3>
-                <button>Terminar mi compra</button>
+                <button onClick={checkout}>Terminar mi compra</button>
             </div>
             </>
              )
