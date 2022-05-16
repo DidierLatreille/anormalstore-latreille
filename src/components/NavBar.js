c5a235851249b5d8005201c5e12ac945d9ef1658
DidierLatreille <didier.latreille@gmail.com>
2022-05-16 03:03:12 -0300

    responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import "../App.css";
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 import CartWidget from "./CartWidget";
 import { Link } from "react-router-dom";
 
@@ -8,23 +8,38 @@ const NavBar = () => {
     return (
         <>
         <Navbar className="NavBar" bg="dark" variant="white">
-            <Container>
+            <Container id="NavBarCont">
                 <Navbar.Brand>
-                <Link style={{textDecoration: "none"}} to='/'><h1 className="TituloNavBar">AnormalStore</h1></Link>
+                <Link style={{textDecoration: "none"}} to='/'><h1 id="TituloNavBar">AnormalStore</h1></Link>
                 </Navbar.Brand>
                 <Nav className="justify-content-center" activeKey="/home">
-                    <Nav.Item>
+                  <Nav.Item className="ItemNavBarFull">
                       <Link to='/category/animales' className="ItemNavBar" style={{textDecoration: "none"}}>Animales</Link>
                     </Nav.Item>
-                    <Nav.Item>
+                    <Nav.Item className="ItemNavBarFull">
                       <Link to='/category/streetPhotography' className="ItemNavBar" style={{textDecoration: "none"}}>StreetPhotography</Link>
                     </Nav.Item>
-                    <Nav.Item>
+                    <Nav.Item className="ItemNavBarFull">
                       <Link to='/category/astros' className="ItemNavBar" style={{textDecoration: "none"}}>Astros</Link>
                     </Nav.Item>
-                    <Nav.Item>
+                    <Nav.Item className="ItemNavBarFull">
                       <Link to='/category/miscelaneo' className="ItemNavBar" style={{textDecoration: "none"}}>Miscelaneo</Link>
                     </Nav.Item>
+
+                    <NavDropdown title="Categorias" id="responsiveNav">
+                        <Nav.Item>
+                          <Link to='/category/animales' className="ItemNavBar" style={{textDecoration: "none"}}>Animales</Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Link to='/category/streetPhotography' className="ItemNavBar" style={{textDecoration: "none"}}>StreetPhotography</Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Link to='/category/astros' className="ItemNavBar" style={{textDecoration: "none"}}>Astros</Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Link to='/category/miscelaneo' className="ItemNavBar" style={{textDecoration: "none"}}>Miscelaneo</Link>
+                        </Nav.Item>
+                    </NavDropdown>
                     <Nav.Item>
                       <CartWidget/>
                     </Nav.Item>
